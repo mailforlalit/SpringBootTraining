0a16ecd337b9f56214acd119fd2c8ce3cbbaba68
LALIT GUPTA <39664049+mailforlalit@users.noreply.github.com>
2026-02-16 00:20:21 +0530

    First Commit

---
+++ src/main/java/com/sbt/learning/dto/ProductDTO.java
@@ -0,0 +1,10 @@
+package com.sbt.learning.dto;
+
+import lombok.Data;
+
+@Data
+public class ProductDTO {
+    private String productName;
+    private String productDescription;
+    private double productPrice;
+}
